1db0445abef135a8bba568ed72ba218d5ebd285a
Superchick-87 <nicolas.peyrebrune@gmail.com>
2026-02-09 14:25:48 +0100

    maj

--- includes/pdf_DateEtHeure.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*----------  Date & heure  ----------*/
-	date_default_timezone_set('Europe/Paris');
-	// echo date("d-m-Y_H:i:s");
-	// $datePdf = date("d-m-Y");
-	$datePdf = date("dmHis");
-	
-
-?>
